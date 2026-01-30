1de1498d806854c8bb73d19fac3a95198a62c692
Lalman <lalmanchaudhary2@gmail.com>
2026-01-30 12:52:27 +0530

    feat: create user model

---
+++ src/modules/auth/models/User.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IUser extends Document {
+  email: String;
+  password: String;
+  createdAt: Date;
+}
+const UserSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model<IUser>("User", UserSchema);
