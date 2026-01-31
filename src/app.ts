ab8fb8f06e326dbac0ec81ea8e3924fa38a27494
Lalman <lalmanchaudhary2@gmail.com>
2026-01-31 18:03:59 +0530

    feat(server): mount job application routes
    - Register application routes under /applications

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./modules/auth/routes/auth.routes.js";
 import { authenticate } from "./modules/auth/middlewares/auth.middleware.js";
+import applicationRoutes from "./modules/applications/routes/application.routes.js";
 
 const app = express();
 
@@ -14,4 +15,6 @@ app.use("/auth", authRoutes);
 app.get("/me", authenticate, (req, res) => {
   res.json({ userId: req.userId });
 });
+
+app.use("/applications", applicationRoutes);
 export default app;
