dd733496f930b2ca4061c9eb3693b9fe8fe3cf37
Lalman <lalmanchaudhary2@gmail.com>
2026-02-16 09:51:52 +0530

    fix(test): configure Jest module resolution for nested TypeScript modules

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,30 @@
 export default {
   testEnvironment: "node",
+
   moduleFileExtensions: ["ts", "js", "json"],
+
   transform: {
-    "^.+\\.(t|j)sx?$": [
+    "^.+\\.(t|j)s$": [
       "@swc/jest",
       {
         jsc: {
           parser: {
             syntax: "typescript",
-            tsx: false,
           },
           target: "es2022",
         },
         module: {
-          type: "es6",
+          type: "commonjs",
         },
       },
     ],
   },
+
+  moduleDirectories: ["node_modules", "src"],
+
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov"],
+
   setupFiles: ["<rootDir>/src/tests/setup.ts"],
 };
